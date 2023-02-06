dc79cdaeaea3729c851c75c541d406c73117d8f3
sistemaswebbrasil <adriano.faria@outlook.com.br>
2023-02-06 06:37:29 -0300

    Adicionando o template principal do https://github.com/mui/material-ui/tree/v5.11.7/docs/data/material/getting-started/templates/dashboard

--- src/pages/persons/index.tsx
+++ src/pages/persons/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Grid } from "@mui/material";
+import { Typography } from "@mui/material";
 
 function persons() {
-  return <div>persons</div>;
+  return (
+    <Grid container rowSpacing={4.5} columnSpacing={2.75}>
+      <Grid item xs={12} sx={{ mb: -2.25 }}>
+        <Typography variant="h5">Lista de Pessoas</Typography>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default persons;
